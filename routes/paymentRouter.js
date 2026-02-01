55232ebd0f435a1547c2501e2a2a6355b768ca65
nebiyuu <nebiyuessayas01@gmail.com>
2026-02-02 02:02:28 +0300

    update,veryfy,redirect payment

--- routes/paymentRouter.js
+++ routes/paymentRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { initiatePayment, verifyPayment } from "../controllers/paymentController.js";
+import { initiatePayment, verifyPayment, handlePaymentRedirect, checkPaymentStatus } from "../controllers/paymentController.js";
 import { requireAuth } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/initiate",requireAuth,initiatePayment);
-router.post("/verify", verifyPayment);
+router.post("/verify", verifyPayment);  
+router.get("/verifyy", handlePaymentRedirect); 
+router.get("/status", checkPaymentStatus);  
 
 export default router;
